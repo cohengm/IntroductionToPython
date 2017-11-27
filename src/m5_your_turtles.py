7797484ab22772344c9c9091f52cfc6aefda701e
cohengm <cohengm@rose-hulman.edu>
2017-11-27 09:30:06 -0500

    finish m3

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Geoffrey Cohen.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,74 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+window.delay(20)
+
+geoff = rg.SimpleTurtle('turtle')
+geoff.pen = rg.Pen('red', 30)
+geoff.speed = 10
+
+geoff.forward(200)
+geoff.right(90)
+geoff.forward(200)
+geoff.right(90)
+geoff.forward(400)
+geoff.right(90)
+geoff.forward(400)
+geoff.right(90)
+geoff.forward(400)
+geoff.right(90)
+
+patrick = rg.SimpleTurtle('turtle')
+patrick.pen = rg.Pen('blue', 30)
+patrick.speed = 10
+
+patrick.backward(20)
+patrick.left(90)
+patrick.forward(20)
+patrick.right(90)
+
+patrick.forward(200)
+patrick.right(90)
+patrick.forward(200)
+patrick.right(90)
+patrick.forward(400)
+patrick.right(90)
+patrick.forward(400)
+patrick.right(90)
+patrick.forward(400)
+patrick.right(90)
+
+patrick.speed = 100
+geoff.speed = 100
+
+distance = 0
+
+for k in range(10):
+    patrick.forward(distance)
+    patrick.right(90)
+    patrick.forward(distance)
+    patrick.right(90)
+    patrick.forward(distance)
+    patrick.right(90)
+    patrick.forward(distance)
+    patrick.right(90)
+    patrick.forward(distance)
+    patrick.right(90)
+
+    geoff.forward(distance)
+    geoff.right(90)
+    geoff.forward(distance)
+    geoff.right(90)
+    geoff.forward(distance)
+    geoff.right(90)
+    geoff.forward(distance)
+    geoff.right(90)
+    geoff.forward(distance)
+    geoff.right(90)
+
+    distance = distance + 10
+
+window.close_on_mouse_click()
